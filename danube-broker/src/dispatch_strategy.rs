4f62f9b4942cd3cc45be941eb1673970c599e4ef
Dan Rusei <dan.rusei@gmail.com>
2025-01-09 18:14:23 +0200

    delete redundant code

--- danube-broker/src/dispatch_strategy.rs
+++ danube-broker/src/dispatch_strategy.rs
@@ -1,7 +1,4 @@
-use danube_client::{ConfigDispatchStrategy, ReliableOptions, RetentionPolicy};
 use danube_reliable_dispatch::ReliableDispatch;
-
-use danube_core::proto::TopicDispatchStrategy;
 
 #[derive(Debug)]
 pub(crate) enum DispatchStrategy {
@@ -9,30 +6,4 @@ pub(crate) enum DispatchStrategy {
     NonReliable,
     // Stores messages for reliable delivery
     Reliable(ReliableDispatch),
-}
-
-impl From<TopicDispatchStrategy> for ConfigDispatchStrategy {
-    fn from(strategy: TopicDispatchStrategy) -> Self {
-        match strategy.strategy {
-            0 => ConfigDispatchStrategy::NonReliable,
-            1 => {
-                if let Some(reliable_opts) = strategy.reliable_options {
-                    let retention_policy = match reliable_opts.retention_policy {
-                        0 => RetentionPolicy::RetainUntilAck,
-                        1 => RetentionPolicy::RetainUntilExpire,
-                        _ => RetentionPolicy::RetainUntilAck,
-                    };
-
-                    ConfigDispatchStrategy::Reliable(ReliableOptions {
-                        segment_size: reliable_opts.segment_size as usize,
-                        retention_policy,
-                        retention_period: reliable_opts.retention_period,
-                    })
-                } else {
-                    ConfigDispatchStrategy::NonReliable
-                }
-            }
-            _ => ConfigDispatchStrategy::NonReliable,
-        }
-    }
 }
