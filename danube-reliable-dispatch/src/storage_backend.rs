ae53a6121940465056bc52e4961af6cd216fad82
Dan Rusei <dan.rusei@gmail.com>
2025-01-10 09:18:53 +0200

    simplify danube-reliable-dispatch

--- danube-reliable-dispatch/src/storage_backend.rs
+++ danube-reliable-dispatch/src/storage_backend.rs
@@ -1,12 +1,8 @@
-use danube_core::storage::{StorageBackend, StorageConfig};
+use async_trait::async_trait;
+use danube_core::storage::{Segment, StorageBackend, StorageConfig};
+use dashmap::DashMap;
 use std::sync::Arc;
-
-mod in_memory;
-pub use in_memory::InMemoryStorage;
-//mod disk;
-//pub use disk::DiskStorage;
-//mod aws_s3;
-//pub use aws_s3::S3Storage;
+use tokio::sync::RwLock;
 
 pub fn create_message_storage(storage_config: &StorageConfig) -> Arc<dyn StorageBackend> {
     match storage_config {
@@ -17,5 +13,45 @@ pub fn create_message_storage(storage_config: &StorageConfig) -> Arc<dyn Storage
         StorageConfig::S3(_s3_config) => {
             todo!()
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct InMemoryStorage {
+    segments: DashMap<usize, Arc<RwLock<Segment>>>,
+}
+
+impl InMemoryStorage {
+    pub fn new() -> Self {
+        Self {
+            segments: DashMap::new(),
+        }
+    }
+}
+
+#[async_trait]
+impl StorageBackend for InMemoryStorage {
+    async fn get_segment(
+        &self,
+        id: usize,
+    ) -> Result<Option<Arc<RwLock<Segment>>>, Box<dyn std::error::Error + Send + Sync>> {
+        Ok(self.segments.get(&id).map(|segment| segment.clone()))
+    }
+
+    async fn put_segment(
+        &self,
+        id: usize,
+        segment: Arc<RwLock<Segment>>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        self.segments.insert(id, segment);
+        Ok(())
+    }
+
+    async fn remove_segment(
+        &self,
+        id: usize,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        self.segments.remove(&id);
+        Ok(())
     }
 }
