1994e26b04d83cea52172e1b6eb900526d38eaa6
Dan Rusei <dan.rusei@gmail.com>
2025-01-10 10:14:25 +0200

    add proto files in danube-core crate

--- danube-core/build.rs
+++ danube-core/build.rs
@@ -5,8 +5,8 @@ use std::path::Path;
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = Path::new("src/proto");
     tonic_build::configure().out_dir(out_dir).compile(
-        &["../proto/DanubeApi.proto", "../proto/DanubeAdmin.proto"],
-        &["../proto"],
+        &["proto/DanubeApi.proto", "proto/DanubeAdmin.proto"],
+        &["proto"],
     )?;
 
     // let client_proto_dir = Path::new("../danube-client/src/proto");
