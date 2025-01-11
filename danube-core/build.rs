d46b615698cc5a70b8fe98918d75685e539df559
Dan Rusei <31576500+danrusei@users.noreply.github.com>
2025-01-11 09:40:28 +0200

    Bump dependencies (#62)
    * bumping the dependencies
    
    * using workspace dependencies

--- danube-core/build.rs
+++ danube-core/build.rs
@@ -4,25 +4,10 @@ use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = Path::new("src/proto");
-    tonic_build::configure().out_dir(out_dir).compile(
+    tonic_build::configure().out_dir(out_dir).compile_protos(
         &["proto/DanubeApi.proto", "proto/DanubeAdmin.proto"],
         &["proto"],
     )?;
-
-    // let client_proto_dir = Path::new("../danube-client/src/proto");
-    // let admin_proto_dir = Path::new("../danube-admin-cli/src/proto");
-
-    // // copy the generated api file to danube-client crate
-    // fs::copy(
-    //     out_dir.join("danube.rs"),
-    //     client_proto_dir.join("danube.rs"),
-    // )?;
-
-    // // copy the generated admin file to danube-admin-cli crate
-    // fs::copy(
-    //     out_dir.join("danube_admin.rs"),
-    //     admin_proto_dir.join("danube_admin.rs"),
-    // )?;
 
     Ok(())
 }
