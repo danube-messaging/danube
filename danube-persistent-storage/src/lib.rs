f93a28a3dddebd4ea041453ecdea35fb27b0e249
Dan Rusei <dan.rusei@gmail.com>
2025-02-08 06:25:25 +0200

    move disk storage code

--- danube-persistent-storage/src/lib.rs
+++ danube-persistent-storage/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod errors;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod local_disk;
+pub use local_disk::DiskStorage;
