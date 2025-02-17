79c5cd6a03a1cd8ca2a956efe410ae8bc96025e1
Dan Rusei <dan.rusei@gmail.com>
2025-02-17 09:11:31 +0200

    connect when is needed to grpc storage backend

--- danube-persistent-storage/src/managed_storage.rs
+++ danube-persistent-storage/src/managed_storage.rs
@@ -17,41 +17,38 @@ use danube_core::managed_storage_proto::{
 
 #[derive(Debug, Clone)]
 pub struct RemoteStorage {
-    client: Arc<Mutex<ManagedStorageClient<Channel>>>,
+    client: Arc<Mutex<Option<ManagedStorageClient<Channel>>>>,
+    connection_config: RemoteStorageConfig,
 }
 
 impl RemoteStorage {
-    pub async fn new(config: &RemoteStorageConfig) -> Result<Self, PersistentStorageError> {
-        let tls_config = if config.use_tls {
-            let ca_cert = std::fs::read(&config.ca_file)?;
-            Some(ClientTlsConfig::new().ca_certificate(Certificate::from_pem(ca_cert)))
-        } else {
-            None
-        };
+    pub fn new(config: RemoteStorageConfig) -> Self {
+        Self {
+            client: Arc::new(Mutex::new(None)),
+            connection_config: config,
+        }
+    }
 
-        // Create connection options
-        let cnx_options = ConnectionOptions {
-            tls_config: tls_config,
-            use_tls: config.use_tls,
-        };
+    async fn ensure_connected(&self) -> Result<(), PersistentStorageError> {
+        let mut client_guard = self.client.lock().await;
+        if client_guard.is_none() {
+            let tls_config = if self.connection_config.use_tls {
+                let ca_cert = std::fs::read(&self.connection_config.ca_file)?;
+                Some(ClientTlsConfig::new().ca_certificate(Certificate::from_pem(ca_cert)))
+            } else {
+                None
+            };
 
-        // Parse endpoint into Uri
-        let uri: Uri = config
-            .endpoint
-            .parse()
-            .expect("Failed to parse endpoint URI");
+            let cnx_options = ConnectionOptions {
+                tls_config,
+                use_tls: self.connection_config.use_tls,
+            };
 
-        // Establish RPC connection using connection.rs
-        let RpcConnection { grpc_cnx } = new_rpc_connection(&cnx_options, &uri)
-            .await
-            .expect("Failed to establish RPC connection");
-
-        // Create client with the established connection
-        let client = ManagedStorageClient::new(grpc_cnx);
-
-        Ok(Self {
-            client: Arc::new(Mutex::new(client)),
-        })
+            let uri: Uri = self.connection_config.endpoint.parse()?;
+            let RpcConnection { grpc_cnx } = new_rpc_connection(&cnx_options, &uri).await?;
+            *client_guard = Some(ManagedStorageClient::new(grpc_cnx));
+        }
+        Ok(())
     }
 }
 
@@ -62,18 +59,28 @@ impl StorageBackend for RemoteStorage {
         topic_name: &str,
         id: usize,
     ) -> Result<Option<Arc<RwLock<Segment>>>, StorageBackendError> {
-        let mut client = self.client.lock().await;
+        self.ensure_connected().await?;
+
+        let mut client_guard = self.client.lock().await;
+        let client = client_guard.as_mut().unwrap();
+
         let request = GetSegmentRequest {
             topic_name: topic_name.to_string(),
             segment_id: id as u64,
         };
 
+        dbg!("Sending GET request...");
+
         match client.get_segment(request).await {
             Ok(response) => {
                 let segment_data = response.into_inner().segment_data;
-                let segment: Segment = bincode::deserialize(&segment_data)
-                    .map_err(|e| StorageBackendError::Managed(e.to_string()))?;
-                Ok(Some(Arc::new(RwLock::new(segment))))
+                if segment_data.is_empty() {
+                    Ok(None)
+                } else {
+                    let segment: Segment = bincode::deserialize(&segment_data)
+                        .map_err(|e| StorageBackendError::Managed(e.to_string()))?;
+                    Ok(Some(Arc::new(RwLock::new(segment))))
+                }
             }
             Err(e) => Err(StorageBackendError::Managed(e.to_string())),
         }
@@ -85,7 +92,11 @@ impl StorageBackend for RemoteStorage {
         id: usize,
         segment: Arc<RwLock<Segment>>,
     ) -> Result<(), StorageBackendError> {
-        let mut client = self.client.lock().await;
+        self.ensure_connected().await?;
+
+        let mut client_guard = self.client.lock().await;
+        let client = client_guard.as_mut().unwrap();
+
         let segment_data = segment.read().await;
         let serialized = bincode::serialize(&*segment_data)
             .map_err(|e| StorageBackendError::Managed(e.to_string()))?;
@@ -96,6 +107,8 @@ impl StorageBackend for RemoteStorage {
             segment_data: serialized,
         };
 
+        dbg!("Sending PUT request...");
+
         match client.put_segment(request).await {
             Ok(_) => Ok(()),
             Err(e) => Err(StorageBackendError::Managed(e.to_string())),
@@ -103,7 +116,11 @@ impl StorageBackend for RemoteStorage {
     }
 
     async fn remove_segment(&self, topic_name: &str, id: usize) -> Result<(), StorageBackendError> {
-        let mut client = self.client.lock().await;
+        self.ensure_connected().await?;
+
+        let mut client_guard = self.client.lock().await;
+        let client = client_guard.as_mut().unwrap();
+
         let request = RemoveSegmentRequest {
             topic_name: topic_name.to_string(),
             segment_id: id as u64,
