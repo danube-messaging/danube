dad97a0122a59d64761ae32df021e07ba3f39515
Dan Rusei <dan.rusei@gmail.com>
2026-03-14 15:08:41 +0200

    remove unused function

--- danube-broker/src/producer.rs
+++ danube-broker/src/producer.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use metrics::gauge;
 use serde::{Deserialize, Serialize};
 
@@ -19,9 +18,7 @@ pub(crate) struct Producer {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[allow(dead_code)]
 pub(crate) enum ProducerAccessMode {
-    // multiple producers can concurrently produce messages to the same topic
     Shared,
-    // nly one producer is allowed to produce messages to the topic
     Exclusive,
 }
 
@@ -40,20 +37,6 @@ impl Producer {
             status: true,
         }
     }
-    // publish message to topic
-    #[allow(dead_code)]
-    pub(crate) async fn publish_message(
-        &self,
-        _producer_id: u64,
-        _message: &Vec<u8>,
-    ) -> Result<()> {
-        // it performs some checks in regards to checksum, encryption etc
-        // and it calls the topic.publish_message()
-
-        //let's assume that the checks pass and let the broker_server to call the topic.publish_message
-
-        Ok(())
-    }
 
     // closes the producer from server-side and inform the client through health_check mechanism
     // to disconnect producer
@@ -62,11 +45,6 @@ impl Producer {
         gauge!(TOPIC_ACTIVE_PRODUCERS.name, "topic" => self.topic_name.to_string()).decrement(1);
 
         self.status = false;
-        self.producer_id
-    }
-
-    #[allow(dead_code)]
-    pub(crate) fn get_id(&self) -> u64 {
         self.producer_id
     }
 }
